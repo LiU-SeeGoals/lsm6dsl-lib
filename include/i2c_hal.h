38c69e894c6b1053d5018e047fc4c4936880c74f
Anton <anton.ostman123@gmail.com>
2025-05-05 18:59:13 +0200

    Add hlindgren stuff

--- include/i2c_hal.h
+++ include/i2c_hal.h
@@ -4,28 +4,23 @@
 #include <stdint.h>
 
 #ifdef __cplusplus
-extern "C"
-{
+extern "C" {
 #endif /* __cplusplus */
-
 
 typedef void Lsm6dsl_I2cPortHandle_t;
 
 void lsm6dsl_i2c_master_init(Lsm6dsl_I2cPortHandle_t *port_handle);
 
+#ifdef LSM6DSL_USE_I2C_MEM_READ_AND_WRITE
+void lsm6dsl_i2c_master_read_mem(
+    Lsm6dsl_I2cPortHandle_t *port_handle, uint8_t device_address,
+    uint8_t mem_address, // Sub addresss within device
+    uint8_t *inbuf, uint8_t size);
 
-#ifdef LSM6DSL_USE_MEM_READ_AND_WRITE
-void lsm6dsl_i2c_master_read_mem(Lsm6dsl_I2cPortHandle_t *port_handle,
-                                 uint8_t device_address,
-                                 uint8_t mem_address, // Sub addresss within device
-                                 uint8_t *inbuf,
-                                 uint8_t size);
-
-void lsm6dsl_i2c_master_write_mem(Lsm6dsl_I2cPortHandle_t *port_handle,
-                                 uint8_t device_address,
-                                 uint8_t mem_address, // Sub address within device
-                                 uint8_t *outbuf,
-                                 uint8_t size);
+void lsm6dsl_i2c_master_write_mem(
+    Lsm6dsl_I2cPortHandle_t *port_handle, uint8_t device_address,
+    uint8_t mem_address, // Sub address within device
+    uint8_t *outbuf, uint8_t size);
 #else
 
 /* void lsm6dsl_i2c_master_transmit(Lsm6dsl_I2cPortHandle_t *port_handle, */
@@ -38,18 +33,16 @@ void lsm6dsl_i2c_master_write_mem(Lsm6dsl_I2cPortHandle_t *port_handle,
 /*                                 uint8_t *inbuf, */
 /*                                 uint8_t size); */
 
-void lsm6dsl_i2c_master_transmit_first_frame(Lsm6dsl_I2cPortHandle_t *port_handle,
-                                             uint8_t device_address,
-                                             uint8_t *outbuf,
-                                             uint8_t size);
+void lsm6dsl_i2c_master_transmit_first_frame(
+    Lsm6dsl_I2cPortHandle_t *port_handle, uint8_t device_address,
+    uint8_t *outbuf, uint8_t size);
 
 /* void lsm6dsl_i2c_master_transmit_next_frame(Lsm6dsl_I2cPortHandle_t
  * *port_handle, uint8_t device_address, uint8_t *outbuf, uint8_t size); */
 
-void lsm6dsl_i2c_master_transmit_last_frame(Lsm6dsl_I2cPortHandle_t *port_handle,
-                                            uint8_t device_address,
-                                            uint8_t *outbuf,
-                                            uint8_t size);
+void lsm6dsl_i2c_master_transmit_last_frame(
+    Lsm6dsl_I2cPortHandle_t *port_handle, uint8_t device_address,
+    uint8_t *outbuf, uint8_t size);
 
 /* void lsm6dsl_i2c_master_receive_first_frame(Lsm6dsl_I2cPortHandle_t
  * *port_handle, uint8_t device_address, uint8_t *inbuf, uint8_t size); */
@@ -59,14 +52,11 @@ void lsm6dsl_i2c_master_transmit_last_frame(Lsm6dsl_I2cPortHandle_t *port_handle
 
 void lsm6dsl_i2c_master_receive_last_frame(Lsm6dsl_I2cPortHandle_t *port_handle,
                                            uint8_t device_address,
-                                           uint8_t *inbuf,
-                                           uint8_t size);
+                                           uint8_t *inbuf, uint8_t size);
 #endif
-
 
 #ifdef __cplusplus
 }
 #endif /* __cplusplus */
 
 #endif // I2C_HAL_H
-
